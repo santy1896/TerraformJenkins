fc02b33a52198eb04d4f0fa48b272bf49ec8bb66
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-02 02:21:40 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,17 +1,17 @@
 
 provider "aws" {
   region  = "ap-south-1"
-  access_key="AKIATX5K52TFQ5ENE66R"
-  secret_key="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"
+  access_key="AKIA3XJDWKEDL3N4MGF7"
+  secret_key="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"
   
 }
 
 resource "aws_instance" "terraform_EC2" {
   ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
-  security_groups=["launch-wizard-3"]
+  security_groups=["launch-wizard-4"]
   associate_public_ip_address = true
-  key_name = "secondawsfile"
+  key_name = "searchheadkey"
   count = 3
 
   tags = {
