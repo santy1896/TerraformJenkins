483a1999b44a73b3ca2326cc08177ae18eb36aba
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-01 21:42:41 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform_EC2" {
-  ami           = "ami-0e742cca61fb65051"
+  ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
   security_groups=["launch-wizard-3"]
   associate_public_ip_address = true
