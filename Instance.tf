b64c0125e95a65468c4f0283a976c4e2ee8ae90b
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-01 21:35:00 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -9,16 +9,15 @@ provider "aws" {
 resource "aws_instance" "terraform_EC2" {
   ami           = "ami-0e742cca61fb65051"
   instance_type = "t2.micro"
-  security_groups=["launch-wizard-1"]
+  security_groups=["launch-wizard-3"]
   associate_public_ip_address = true
   key_name = "KeyGudoo"
   count = 3
 
   tags = {
-    Name ="SERVER01"
+    Name ="TestingServer"
     Environment = "DEV"
-    OS = "UBUNTU"
-    Managed = "IAC"
+    OS = "Linux"
   }
 
   }
