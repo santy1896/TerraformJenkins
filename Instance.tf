70d15d4d153180ffdf908489fcf0ff13c94af890
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-02 19:20:39 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -9,9 +9,9 @@ provider "aws" {
 resource "aws_instance" "terraform_EC2" {
   ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
-  security_groups=["launch-wizard-4"]
+  security_groups=["launch-wizard-3"]
   associate_public_ip_address = true
-  key_name = "searchheadkey"
+  key_name = "secondawsfile"
   count = 3
 
   tags = {
